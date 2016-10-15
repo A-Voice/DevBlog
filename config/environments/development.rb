63b949d44b4578eb317b43385985e61b89843bb2
A. V. Edwards <avedesigns@outlook.com>
2016-10-14 17:56:26 -0700

    Added CRUD

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  Paperclip.options[:command_path] = "/c/WINDOWS/system32/convert"
+  
 end
